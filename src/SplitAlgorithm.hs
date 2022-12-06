7304ce3990631f8af131dddadffc1106a39914c8
Hao Feng <gilone@foxmail.com>
2022-12-06 13:08:44 -0800

    add another example

--- src/SplitAlgorithm.hs
+++ src/SplitAlgorithm.hs
@@ -117,8 +117,6 @@ updateBalance balanceMap maxPayerTuple maxReciverTuple =
 
 -- getSuggestionsDFS :: [MD.ExpenseRecord] -> [MD.SplitSuggestion]
 
-
-
 --     def minTransfers(self, transactions: List[List[int]]) -> int:
 --         balances = getTheBalanceMap(transactions)
 
@@ -159,6 +157,29 @@ updateBalance balanceMap maxPayerTuple maxReciverTuple =
         
 --         explore_subgroups()
 --         return len(balances) - to_update['max_num_subgroups']
+
+
+-- def minTransfers(self, transactions):
+--     counter = collections.Counter()
+--     for f, t, m in transactions:
+--         counter[f] -= m
+--         counter[t] += m
+--     balances = counter.values()
+--     def dfs(b):
+--         if not b:
+--             return 0
+--         if not b[0]:
+--             return dfs(b[1:])
+--         for i in range(1, len(b)):
+--             if b[i] == -b[0]:
+--                 return 1 + dfs(b[1:i] + [0] + b[i+1:])
+--         ret = []
+--         for i in range(1, len(b)):
+--             if b[i] * b[0] < 0:
+--                 ret.append(dfs(b[1:i] + [b[i]+b[0]] + b[i+1:]))
+--         return 1+min(ret)
+    
+--     return dfs(balances)
 
 
 -- reference
